a24bcb5a694ffbb06c24542740713aad55467a92
donguk585 <donguk585@gmail.com>
2023-07-20 21:49:05 +0900

    sts4 기본설정

--- gjw.project/src/main/java/com/omt/gjw/project/config/auth/AuthFailureHandler.java
+++ gjw.project/src/main/java/com/omt/gjw/project/config/auth/AuthFailureHandler.java
@@ -1,5 +1,24 @@
 package com.omt.gjw.project.config.auth;
 
-public class AuthFailureHandler {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class AuthFailureHandler implements AuthenticationFailureHandler{
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		
+		System.out.println(exception.getMessage());
+		response.setContentType("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		response.getWriter().print("<html><head></head><body><script>alert(\"로그인실패\");history.back();</script></body></html>");
+	}
 
 }
