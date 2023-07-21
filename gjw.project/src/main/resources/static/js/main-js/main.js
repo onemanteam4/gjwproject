32e978400bd0ee876770c1d32167fc7af4128760
doyoujun <rntlrtlffur95@naver.com>
2023-07-21 17:31:20 +0900

    main-js 및 슬라이더

--- gjw.project/src/main/resources/static/js/main-js/main.js
+++ gjw.project/src/main/resources/static/js/main-js/main.js
@@ -2,14 +2,7 @@ const operationTimeCalendar = document.querySelector(".operation-time-calendar")
 const viewCalendar = document.querySelector(".view-calendar");
 
 
-//캘린더 버튼 클릭시 나오게
-viewCalendar.onclick = () => {
-    if(operationTimeCalendar.style.display == 'none') {
-        operationTimeCalendar.style.display = 'block';
-    }else {
-        operationTimeCalendar.style.display = 'none';
-    }
-}
+
 
 const listHover = document.querySelector(".list-hover");
 const hover = document.querySelectorAll(".hover");
@@ -83,8 +76,12 @@ function banner () {
         index ++;
         
         console.log(index);
-        if( index >= 4) {
+        if( index >= 5) {
             index = -1;
+            setTimeout(() => {
+                boxSlider.style.transition = 'transform 0s ease-out';
+                boxSlider.style.transform = 'translate3d( 0px, 0px, 0px)';
+            }, 1010)
         }
     }, 10000);
        
@@ -92,5 +89,171 @@ function banner () {
 }
 
 
+//메인 슬라이드
+const centerMainBannerList = document.querySelector(".center-main-banner-list");
+const bannerList = document.querySelectorAll(".center-main-banner-list li");
+const sliderPre = document.querySelector(".sliderPre");
+const sliderNext = document.querySelector(".sliderNext");
+let x = centerMainBannerList.style.transform;
+let result = x.substring(x.lastIndexOf("-")+1, 17);
+mainbanner();
+
+function mainbanner () {
+    // for(let i = 0; i < boxSliderList.length; i++) {
+    //     imgCount++;
+    // }
+
+    let index = 0;
+
+    let time = setInterval( () => {
+        
+
+        centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+        centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index+1) + 'px, 0px, 0px)';
+        index ++;
+        
+        console.log(index+1);
+        if( index >= 7) {
+            index = -1;
+
+            setTimeout(() => {
+                centerMainBannerList.style.transition = 'transform 0s ease-out';
+                centerMainBannerList.style.transform = 'translate3d( -3900px, 0px, 0px)';
+            }, 501)
+        }
+    }, 10000);
+       
+    centerMainBannerList.onmouseover = () => {
+        clearInterval(time);
+        console.log("멈춰!");
+    }
+
+    centerMainBannerList.onmouseout = () => {
+
+        index = 0;
+        time = setInterval( () => {
+            console.log("재시작!");
+
+            centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+            centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index+1) + 'px, 0px, 0px)';
+            index ++;
+            
+            console.log(index+1);
+            if( index >= 7) {
+                index = -1;
+    
+                setTimeout(() => {
+                    centerMainBannerList.style.transition = 'transform 0s ease-out';
+                    centerMainBannerList.style.transform = 'translate3d( -3900px, 0px, 0px)';
+                }, 501)
+            }
+        }, 10000);
+    }
+}
+
+let currentIndex = 0;
 
 
+
+sliderPre.onclick = () => {
+    currentIndex --;
+    currentIndex = currentIndex < 0 ? 0 : currentIndex;
+    centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+    centerMainBannerList.style.transform ='translate3d (-' + result + 'px, 0px, 0px)';
+    console.log(centerMainBannerList.style.transform ='translate3d(-' + result + 'px, 0px, 0px)');
+
+    console.log(centerMainBannerList.style.transform = 'translate3d(-'+ 1300*2 + 'px, 0px, 0px)');
+}
+
+
+let index = 0;
+sliderNext.onclick = () => {
+    
+    index ++;
+    centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+    centerMainBannerList.style.transform = 'translate3d(-'+ 1300*index + 'px, 0px, 0px)';
+    if(index >= 7) {
+        index=3
+    }
+    console.log("ㅇㅇ" + index);
+}
+
+//어트랙션 슬라이드
+
+const AttractionboxSlider = document.querySelector(".box-slider");
+const AttractionList = document.querySelectorAll(".box-slider li");
+
+attractionbanner();
+
+function attractionbanner () {
+    // for(let i = 0; i < boxSliderList.length; i++) {
+    //     imgCount++;
+    // }
+
+    let index = 0;
+
+    setInterval( () => {
+        
+
+        AttractionboxSlider.style.transition = 'transform 0.5s ease-out';
+        AttractionboxSlider.style.transform = 'translate3d(-' + 1225*(index+1) + 'px, 0px, 0px)';
+        index ++;
+        
+        console.log(index);
+        if( index >= bannerList.length-4) {
+            index = -1;
+
+            setTimeout(() => {
+                AttractionboxSlider.style.transition = 'transform 0s ease-out';
+                AttractionboxSlider.style.transform = 'translate3d( 0px, 0px, 0px)';
+            }, 1001)
+
+        }
+    }, 5000);
+       
+
+}
+
+
+
+
+
+
+
+
+
+
+//눈으로 보는 가이드 슬라이드
+
+const guideSlider = document.querySelector(".guide-slider");
+const guideList = document.querySelectorAll(".guide-slider li");
+
+guidebanner();
+
+function guidebanner () {
+    // for(let i = 0; i < boxSliderList.length; i++) {
+    //     imgCount++;
+    // }
+
+    let index = 0;
+
+    setInterval( () => {
+        
+
+        guideSlider.style.transition = 'transform 1s ease-out';
+        guideSlider.style.transform = 'translate3d(-' + 936*(index+1) + 'px, 0px, 0px)';
+        index ++;
+        
+        console.log(index);
+        if( index >= 4) {
+            index = -1;
+
+            setTimeout(() => {
+                guideSlider.style.transition = 'transform 0s ease-out';
+                guideSlider.style.transform = 'translate3d( 0px, 0px, 0px)';
+            }, 1001)
+        }
+    }, 5000);
+       
+
+}
