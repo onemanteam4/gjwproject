c9985930d6656136b496d4bc27478f257e3df0d6
doyoujun <rntlrtlffur95@naver.com>
2023-08-10 17:58:18 +0900

    메인

--- gjw.project/src/main/resources/static/js/main-js/main.js
+++ gjw.project/src/main/resources/static/js/main-js/main.js
@@ -56,6 +56,22 @@ quickController.onmouseout = () => {
 //맨 위 배너 슬라이드
 const boxSlider = document.querySelector(".box-slider-list");
 const boxSliderList = document.querySelectorAll(".box-slider-list li");
+
+const boxPagerLink = document.querySelectorAll(".box-pager div a");
+
+console.log(boxPagerLink.length);
+
+
+for(let x = 0; x < boxPagerLink.length; x++) {
+	boxPagerLink[x].onclick = () => {
+		boxSlider.style.transition = 'transform 0.5s ease-out';
+        boxSlider.style.transform = 'translate3d(-' + 1903*x + 'px, 0px, 0px)';
+	}
+}
+
+
+
+
 let imgCount = 0;
 let slideCount = boxSliderList.length;
 
@@ -124,6 +140,8 @@ const bannerList = document.querySelectorAll(".center-main-banner-list li");
 const sliderPre = document.querySelector(".sliderPre");
 const sliderNext = document.querySelector(".sliderNext");
 
+const MainSliderBtn = document.querySelector(".main-slider-btn");
+
 mainbanner();
 
 function mainbanner () {
@@ -131,22 +149,69 @@ function mainbanner () {
     //     imgCount++;
     // }
 
-    let index = 0;
+	let index = 1;
+    
 
     let time = setInterval( () => {
         
 
         centerMainBannerList.style.transition = 'transform 0.5s ease-out';
-        centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index+1) + 'px, 0px, 0px)';
+        centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index) + 'px, 0px, 0px)';
+        
+		sliderPre.onclick = () => {
+			console.log("이건?!" + index);
+	    	index = index - 1;
+			console.log("이건?!!" + index);
+			
+			
+			centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+	    	centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index + -1)+ 'px, 0px, 0px)';
+	    	console.log("이건?!!!!" + index);
+	    if(index <= 2) {
+	        index=10;
+	        
+                setTimeout(() => {
+			centerMainBannerList.style.transition = 'transform 0s ease-out';
+	    	centerMainBannerList.style.transform = 'translate3d(-11700px, 0px, 0px)';
+                }, 530)
+	        
+	    }
+	}
+        
+        
+        
+        
+        
         index ++;
         
+    	sliderNext.onclick = () => {
+    		
+		console.log(index);
+		centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+		centerMainBannerList.style.transform = 'translate3d(-'+ 1300*index + 'px, 0px, 0px)';
+		
+		index = index + 1;
+		console.log("이건?" + index);
+		if(index >= 10) {
+		    index=1;
+		    
+                setTimeout(() => {
+                    centerMainBannerList.style.transition = 'transform 0s ease-out';
+                    centerMainBannerList.style.transform = 'translate3d( -1300px, 0px, 0px)';
+                }, 550)
+    }
+}
+		
+
+
+		
         console.log(index+1);
-        if( index >= 7) {
-            index = -1;
+        if( index >= 9) {
+            index = 1;
 
             setTimeout(() => {
                 centerMainBannerList.style.transition = 'transform 0s ease-out';
-                centerMainBannerList.style.transform = 'translate3d( 0px, 0px, 0px)';
+                centerMainBannerList.style.transform = 'translate3d( -1300px, 0px, 0px)';
             }, 501)
         }
     }, 10000);
@@ -155,24 +220,51 @@ function mainbanner () {
         clearInterval(time);
         console.log("멈춰!");
     }
+    
+    MainSliderBtn.onmouseover = () => {
+        clearInterval(time);
+        console.log("멈춰!");
+    }
 
     centerMainBannerList.onmouseout = () => {
-
-        index = 0;
+		
+        
         time = setInterval( () => {
-            console.log("재시작!");
-
+			
+			
+            console.log("재시작 메인!" + index);
             centerMainBannerList.style.transition = 'transform 0.5s ease-out';
-            centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index+1) + 'px, 0px, 0px)';
+            centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index) + 'px, 0px, 0px)';
             index ++;
             
             console.log(index+1);
-            if( index >= 7) {
-                index = -1;
+            if( index >= 10) {
+                index = 1;
     
                 setTimeout(() => {
                     centerMainBannerList.style.transition = 'transform 0s ease-out';
-                    centerMainBannerList.style.transform = 'translate3d( 0px, 0px, 0px)';
+                    centerMainBannerList.style.transform = 'translate3d( -1300px, 0px, 0px)';
+                }, 501)
+            }
+        }, 10000);
+    }
+        MainSliderBtn.onmouseout = () => {
+
+
+        time = setInterval( () => {
+			
+            console.log("재시작 버튼!" + index);
+            centerMainBannerList.style.transition = 'transform 0.5s ease-out';
+            centerMainBannerList.style.transform = 'translate3d(-'+ 1300*(index) + 'px, 0px, 0px)';
+            index ++;
+            
+            console.log(index+1);
+            if( index >= 10) {
+                index = 1;
+    
+                setTimeout(() => {
+                    centerMainBannerList.style.transition = 'transform 0s ease-out';
+                    centerMainBannerList.style.transform = 'translate3d( -1300px, 0px, 0px)';
                 }, 501)
             }
         }, 10000);
@@ -183,7 +275,7 @@ let currentIndex = 0;
 
 
 
-sliderPre.onclick = () => {
+/*sliderPre.onclick = () => {
     currentIndex --;
     currentIndex = currentIndex < 0 ? 0 : currentIndex;
     centerMainBannerList.style.transition = 'transform 0.5s ease-out';
@@ -191,28 +283,20 @@ sliderPre.onclick = () => {
     console.log(centerMainBannerList.style.transform ='translate3d(-' + result + 'px, 0px, 0px)');
 
     console.log(centerMainBannerList.style.transform = 'translate3d(-'+ 1300*2 + 'px, 0px, 0px)');
-}
+}*/
 
-let index = 0;
-sliderNext.onclick = () => {
-    
-    index ++;
-    
-    let int = (-1300*index)
-    
-    console.log(index);
-    centerMainBannerList.style.transition = 'transform 0.5s ease-out';
-    centerMainBannerList.style.transform = 'translate3d(-'+ 1300*index + 'px, 0px, 0px)';
-    if(index >= 7) {
-        index=1
-    }
-    console.log("ㅇㅇ" + index);
-}
+
+
+
+
 
 //어트랙션 슬라이드
 
 const AttractionboxSlider = document.querySelector(".box-slider");
 const AttractionList = document.querySelectorAll(".box-slider li");
+const boxPre = document.querySelector(".box-pre");
+const boxNext = document.querySelector(".box-next");
+const boxs = document.querySelector(".main-attraction-buttons");
 
 attractionbanner();
 
@@ -227,22 +311,59 @@ function attractionbanner () {
         
 
         AttractionboxSlider.style.transition = 'transform 0.5s ease-out';
-        AttractionboxSlider.style.transform = 'translate3d(-' + 1225*(index+1) + 'px, 0px, 0px)';
-        index ++;
+        AttractionboxSlider.style.transform = 'translate3d(-' + 1225*(index) + 'px, 0px, 0px)';
         
+        boxPre.onclick = () => {
+			console.log("과연?!! " + index);
+			index = index - 1;
+			console.log("과연?!! " + index);
+			AttractionboxSlider.style.transition = 'transform 0.5s ease-out';
+        	AttractionboxSlider.style.transform = 'translate3d(-' + 1225*(index) + 'px, 0px, 0px)';
+		
+			
+			if(index <= -1) {
+				AttractionboxSlider.style.transition = 'transform 0s ease-out';
+                AttractionboxSlider.style.transform = 'translate3d( -7350px, 0px, 0px)';
+                index = 6;
+			}
+		}
+        
+        
+        
+        index ++;
         console.log(index);
-        if( index >= bannerList.length-4) {
-            index = -1;
+        boxNext.onclick = () => {
+			AttractionboxSlider.style.transition = 'transform 0.5s ease-out';
+        	AttractionboxSlider.style.transform = 'translate3d(-' + 1225*(index) + 'px, 0px, 0px)';
+        	
+        	
+        	index = index + 1;
+        	console.log("과연?" + index);
+        	if(index >= 7) {
+				setTimeout(() => {
+                	AttractionboxSlider.style.transition = 'transform 0s ease-out';
+                	AttractionboxSlider.style.transform = 'translate3d( 0px, 0px, 0px)';
+            		}, 530)
+                index = 0;
+			}
+ 			
+		}
+		
+		
+		
+        console.log("과연?!" + index);
+        if( index >= 7) {
+            index = 0;
 
             setTimeout(() => {
                 AttractionboxSlider.style.transition = 'transform 0s ease-out';
                 AttractionboxSlider.style.transform = 'translate3d( 0px, 0px, 0px)';
-            }, 1001)
+            }, 550)
 
         }
-    }, 5000);
-       
+    }, 10000);
 
+	
 }
 
 
