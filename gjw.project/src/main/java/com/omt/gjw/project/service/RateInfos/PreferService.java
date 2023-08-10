57bd51e55e43d3207d2920ed99892130f8efd83f
coffeegoodzz <ejsvk9811@naver.com>
2023-08-10 20:24:21 +0900

    8.10

---
+++ gjw.project/src/main/java/com/omt/gjw/project/service/RateInfos/PreferService.java
@@ -0,0 +1,9 @@
+package com.omt.gjw.project.service.RateInfos;
+
+import java.util.List;
+
+import com.omt.gjw.project.web.dto.rate.PreferRespDto;
+
+public interface PreferService {
+	public List<PreferRespDto> getprerferList() throws Exception;
+}
